26bfff1f12467d635468e647663969e810258a7a
Akbar <akbar.code20@gmail.com>
2026-05-02 09:40:38 +0530

    deployment

--- project-manager/frontend/src/services/api.js
+++ project-manager/frontend/src/services/api.js
@@ -1,17 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://project-management-production-0360.up.railway.app",
 });
 
-// attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
-
   if (token) {
     req.headers.Authorization = `Bearer ${token}`;
   }
-
   return req;
 });
 
